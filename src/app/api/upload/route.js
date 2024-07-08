b333d772fdec18e3e9b835634b405895cbd2a8d8
KARTIKEY-KATYAL <107783109+KARTIKEY-KATYAL@users.noreply.github.com>
2024-07-08 10:57:53 +0530

    c2

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -8,10 +8,10 @@ export async function POST(req) {
     const file = data.get('file');
 
     const s3Client = new S3Client({
-      region: 'us-east-1',
+      region: "ap-southeast-2",
       credentials: {
-        accessKeyId: process.env.MY_AWS_ACCESS_KEY,
-        secretAccessKey: process.env.MY_AWS_SECRET_KEY,
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
       },
     });
 
@@ -24,7 +24,7 @@ export async function POST(req) {
     }
     const buffer = Buffer.concat(chunks);
 
-    const bucket = 'dawid-food-ordering';
+    const bucket = 'quickcrust';
     await s3Client.send(new PutObjectCommand({
       Bucket: bucket,
       Key: newFileName,
--- src/models/MenuItem.js
+++ src/models/MenuItem.js
@@ -5,14 +5,17 @@ const ExtraPriceSchema = new Schema({
   price: Number,
 });
 
-const MenuItemSchema = new Schema({
-  image: {type: String},
-  name: {type: String},
-  description: {type: String},
-  category: {type: mongoose.Types.ObjectId},
-  basePrice: {type: Number},
-  sizes: {type:[ExtraPriceSchema]},
-  extraIngredientPrices: {type:[ExtraPriceSchema]},
-}, {timestamps: true});
+const MenuItemSchema = new Schema(
+  {
+    image: { type: String },
+    name: { type: String },
+    description: { type: String },
+    category: { type: mongoose.Types.ObjectId, default: null, sparse: true },
+    basePrice: { type: Number },
+    sizes: { type: [ExtraPriceSchema] },
+    extraIngredientPrices: { type: [ExtraPriceSchema] },
+  },
+  { timestamps: true }
+);
 
 export const MenuItem = models?.MenuItem || model('MenuItem', MenuItemSchema);
